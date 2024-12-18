b44148bf9b26262b4cb97624ff3b6585d2e67d28
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-18 17:10:09 +0700

    membuat data class Dokter

---
+++ app/src/main/java/com/example/ucp2/data/entity/Dokter.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2.data.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "dokter")
+data class Dokter(
+    @PrimaryKey
+    val id: String,
+    val nama: String,
+    val spesialis: String,
+    val klinik: String,
+    val nohp: String,
+    val jamPraktik : String
+)
