43e7fa8a72aef3fd44a486a313058761251c14db
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-23 22:25:50 +0700

    membuat interface dokterdao dengan methode insert dan query

---
+++ app/src/main/java/com/example/ucp2/data/dao/DokterDao.kt
@@ -0,0 +1,18 @@
+package com.example.ucp2.data.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.ucp2.data.entity.Dokter
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface DokterDao {
+    @Insert
+    suspend fun insertDokter(dokter: Dokter)
+
+    @Query("SELECT * FROM dokter ORDER BY nama ASC")
+    fun getAllDokter(): Flow<List<Dokter>>
+
+}
