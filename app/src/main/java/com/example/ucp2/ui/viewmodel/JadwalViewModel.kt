4cf1c5bac0c85a7fed6181e95482666d478cfca7
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 03:11:44 +0700

    menambahkan function adwalEvent.toJadwalEntity

--- app/src/main/java/com/example/ucp2/ui/viewmodel/JadwalViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/JadwalViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Jadwal
+
 data class JadwalEvent(
     val id: String = "",
     val NamaDokter: String = "",
@@ -7,4 +9,12 @@ data class JadwalEvent(
     val noHp: String = "",
     val tanggalKonsultasi: String = "",
     val status: String = ""
+)
+fun JadwalEvent.toJadwalEntity(): Jadwal = Jadwal(
+    id = id,
+    namaDokter = NamaDokter,
+    namaPasien = NamaPasien,
+    noHp = noHp,
+    tanggalKonsultasi = tanggalKonsultasi,
+    status = status
 )
