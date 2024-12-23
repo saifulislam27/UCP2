218b7c91db1735a3ce396df3950d789f6d0bbb44
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 02:24:42 +0700

    membuat class HomeDktViewModel untuk mengatur tapilan ui home dokter

--- app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDktViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/HomeDktViewModel.kt
@@ -1,7 +1,52 @@
 package com.example.ucp2.ui.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ucp2.data.entity.Dokter
+import com.example.ucp2.repository.RepositoryDkt
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.filterNotNull
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.stateIn
 
+class HomeDktViewModel(
+    private val repositoryDr: RepositoryDkt
+) : ViewModel() {
+
+    val homeDktUiState: StateFlow<HomeDktUiState> = repositoryDr.getAllDkt()
+        .filterNotNull()
+        .map {
+            HomeDktUiState(
+                listDr = it.toList(),
+                isLoading = false,
+            )
+        }
+        .onStart {
+            emit(HomeDktUiState(isLoading = true))
+            delay(900)
+        }
+        .catch {
+            emit(
+                HomeDktUiState(
+                    isLoading = false,
+                    isError = true,
+                    errorMessage = it.message ?: "Terjadi Kesalahan"
+                )
+            )
+        }
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = HomeDktUiState(
+                isLoading = true
+            )
+        )
+
+}
 data class HomeDktUiState(
     val listDr: List<Dokter> = listOf(),
     val isLoading: Boolean = false,
