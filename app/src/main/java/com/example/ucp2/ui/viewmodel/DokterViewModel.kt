e71ef019286da60743f7deaa19245c23ac239684
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 02:35:03 +0700

    membuat data class DokterEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DokterViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodel
+
+data class DokterEvent(
+    var id: String = "",
+    var nama: String = "",
+    var spesialis: String = "",
+    var klinik: String = "",
+    var noHp: String = "",
+    var jamKerja: String = ""
+)
