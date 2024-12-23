1c7600506aea6e94848e834ddbf74b903370c24a
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 02:38:50 +0700

    menambahkan function DokterEvent.toDokterEntity untuk menyimpan hasil imputan

--- app/src/main/java/com/example/ucp2/ui/viewmodel/DokterViewModel.kt
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/DokterViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ucp2.ui.viewmodel
 
+import com.example.ucp2.data.entity.Dokter
+
 data class DokterEvent(
     var id: String = "",
     var nama: String = "",
@@ -7,4 +9,12 @@ data class DokterEvent(
     var klinik: String = "",
     var noHp: String = "",
     var jamKerja: String = ""
+)
+fun DokterEvent.toDokterEntity(): Dokter = Dokter(
+    id = id,
+    nama = nama,
+    spesialis = spesialis,
+    klinik = klinik,
+    noHp = noHp,
+    jamKerja = jamKerja
 )
