18d7b2e90cf915211934f3a07016b07565e9a06d
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 03:08:41 +0700

    membuat data class JadwalEvent

---
+++ app/src/main/java/com/example/ucp2/ui/viewmodel/JadwalViewModel.kt
@@ -0,0 +1,10 @@
+package com.example.ucp2.ui.viewmodel
+
+data class JadwalEvent(
+    val id: String = "",
+    val NamaDokter: String = "",
+    val NamaPasien: String = "",
+    val noHp: String = "",
+    val tanggalKonsultasi: String = "",
+    val status: String = ""
+)
