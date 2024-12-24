2c2460ec103a40d8818ba2e1d74ac83471d74e72
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 13:58:08 +0700

    membuat @composable dan function DeleteConfirmationDialog, menambah alertdiaolog untuk meminta request dan membuat button

---
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJdwView.kt
@@ -0,0 +1,31 @@
+package com.example.ucp2.ui.view.jadwal
+
+import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+private fun DeleteConfirmationDialog(
+    onDeleteConfirm: () -> Unit,
+    onDeleteCancel: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    AlertDialog(
+        onDismissRequest = { },
+        title = { Text("Delete Data") },
+        text = { Text("Anda yakin ingin menghapus data ini?") },
+        modifier = modifier,
+        dismissButton = {
+            TextButton(onClick = onDeleteCancel) {
+                Text(text = "Batal")
+            }
+        },
+        confirmButton = {
+            TextButton(onClick = onDeleteConfirm) {
+                Text(text = "Ya")
+            }
+        }
+    )
+}
