3b8f4d73ac82a588d8c4e35567ebc1aaaf54af7e
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 15:32:23 +0700

    membuat @composable dan function InsertJdwView, menambahkan TopAppBar di dalam column

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/InsertJdwView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/InsertJdwView.kt
@@ -2,29 +2,88 @@ package com.example.ucp2.ui.view.jadwal
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarHost
+import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ucp2.ui.costumwidget.DynamicSelectedTextField
+import com.example.ucp2.ui.costumwidget.TopAppBar
 import com.example.ucp2.ui.navigation.AlamatNavigasi
 import com.example.ucp2.ui.viewmodel.FormErrorJadwalState
 import com.example.ucp2.ui.viewmodel.JadwalEvent
 import com.example.ucp2.ui.viewmodel.JadwalViewModel
 import com.example.ucp2.ui.viewmodel.JdwUiState
 import com.example.ucp2.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
+
+@Composable
+fun InsertJdwView(
+    onBack: () -> Unit,
+    onNavigate: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: JadwalViewModel = viewModel(factory = PenyediaViewModel.Factory)
+){
+    val JdwState = viewModel.JdwUiState
+    val snackBarHostState = remember { SnackbarHostState() }
+    val coroutineScope = rememberCoroutineScope()
+
+    LaunchedEffect(JdwState.snackbarMessage) {
+        JdwState.snackbarMessage?.let { message ->
+            coroutineScope.launch {
+                snackBarHostState.showSnackbar(message)
+                viewModel.resetSnackbarMessage()
+            }
+        }
+    }
+    Scaffold (
+        modifier = modifier,
+        snackbarHost = { SnackbarHost(hostState = snackBarHostState) }
+
+    ){padding ->
+        Column (
+            modifier = modifier
+                .fillMaxSize()
+                .padding(padding)
+                .padding(16.dp)
+        ){
+            TopAppBar(
+                onBack = onBack,
+                showBackButton = true,
+                judul = "Tambah Jadwal"
+            )
+            InsertBodyJadwal(
+                JdwState = JdwState,
+                onValueChange = {updateEvent ->
+                    viewModel.updateJadState(updateEvent)
+                },onClick = {
+                    coroutineScope.launch {
+                        viewModel.saveData()
+                    }
+                    onNavigate()
+                }
+            )
+        }
+    }
+}
 
 @Composable
 fun InsertBodyJadwal(
