c3b466ba150a2f771b54e07128b587c46e65c87c
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 13:32:42 +0700

    membuat @composable dan function ListJadwal

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJdwView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/HomeJdwView.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.DateRange
@@ -20,6 +22,28 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.ucp2.data.entity.Jadwal
+import com.example.ucp2.repository.RepositoryJdw
+
+@Composable
+fun ListJadwal(
+    listJdw: List<Jadwal>,
+    modifier: Modifier = Modifier,
+    onClick: (String) -> Unit = { }
+) {
+    LazyColumn(
+        modifier = modifier
+    ) {
+            items(
+                items = listJdw,
+                itemContent = { jdw ->
+                    CardJdw(
+                        Jdw = jdw,
+                        onClick = { onClick(jdw.id) }
+                )
+            }
+        )
+    }
+}
 
 @Composable
 fun CardJdw (
