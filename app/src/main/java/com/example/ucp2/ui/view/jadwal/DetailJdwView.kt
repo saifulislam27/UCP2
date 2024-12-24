2b01a97d101a21114c5cfc9186d476cbcf7fe8d8
saifulislam27 <mariyatilambeja@gmail.com>
2024-12-24 14:03:58 +0700

    membuat @composable dan function ItemDetailJdw membuat column dan memberikan spacer di masing masing ComponentDetailJdw

--- app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJdwView.kt
+++ app/src/main/java/com/example/ucp2/ui/view/jadwal/DetailJdwView.kt
@@ -1,8 +1,11 @@
 package com.example.ucp2.ui.view.jadwal
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.AlertDialog
+import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
@@ -10,7 +13,44 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.ucp2.data.entity.Jadwal
+
+@Composable
+fun ItemDetailJdw(
+    modifier: Modifier = Modifier,
+    jadwal: Jadwal
+) {
+    Card (
+        modifier = modifier
+            .fillMaxWidth(),
+    ) {
+        Column (
+            modifier = Modifier.padding(16.dp),
+        ){
+            ComponentDetailJdw(judul = "Id", isinya = jadwal.id)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailJdw(judul = "Nama Dokter", isinya = jadwal.namaDokter)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailJdw(judul = "Nama Pasien", isinya = jadwal.namaPasien)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailJdw(judul = "Nomor hp", isinya = jadwal.noHp)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+
+            ComponentDetailJdw(judul = "Tanggal Konsultasi", isinya = jadwal.tanggalKonsultasi)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+            ComponentDetailJdw(judul = "Status", isinya = jadwal.status)
+            Spacer(modifier = Modifier.padding(4.dp))
+
+        }
+    }
+}
 
 @Composable
 fun ComponentDetailJdw(
